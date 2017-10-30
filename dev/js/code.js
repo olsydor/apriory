73daa8863ab03fa356e1523816013eb5e2a8dc49
unknown <mr.1bilous@gmail.com>
2017-10-30 22:17:28 +0200

    commit for change brange

--- dev/js/code.js
+++ dev/js/code.js
@@ -1,13 +1,14 @@
-
+$(document).ready(function(){
+});
 function initMap() {
-	var uluru = {lat: 55.7412458, lng: 37.6044463};
-	var mark_in_map = new google.maps.LatLng(55.7466322,37.6341819);
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom:14,
-	  center: uluru
-	});
-	var marker = new google.maps.Marker({
-	  position: mark_in_map,
-	  map: map
-	});	
-}
+        var uluru = {lat: 55.7412458, lng: 37.6044463};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom:14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+		
+    }
