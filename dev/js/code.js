a4d290d4b5d389b9c4238aa4e3aa66da08713a48
unknown <mr.1bilous@gmail.com>
2017-11-18 11:28:19 +0200

    pull to local

--- dev/js/code.js
+++ dev/js/code.js
@@ -11,3 +11,72 @@ function initMap() {
 	  map: map
 	});	
 }
+$(document).ready(function(){
+	$('#show_0').click(function(){
+		$("#det_0").slideDown(1000);
+		$(this).hide()
+		$("#hide_0").show()	
+	});
+	$('#hide_0').click(function(){
+		$("#det_0").slideUp(1000);
+		$(this).hide()
+		$("#show_0").show()	
+	});
+	
+	$('#show_1').click(function(){
+		$("#det_1").slideDown(1000);
+		$(this).hide()
+		$("#hide_1").show()		
+	});
+	$('#hide_1').click(function(){
+		$("#det_1").slideUp(1000);
+		$(this).hide()
+		$("#show_1").show()	
+	});
+	
+	$('#show_2').click(function(){
+		$("#det_2").slideDown(1000);
+		$(this).hide()
+		$("#hide_2").show()	
+	});
+	$('#hide_2').click(function(){
+		$("#det_2").slideUp(1000);
+		$(this).hide()
+		$("#show_2").show()
+	});
+	
+	$('#show_3').click(function(){
+		$("#det_3").slideDown(1000);
+		$(this).hide()
+		$("#hide_3").show()	
+	});
+	$('#hide_3').click(function(){
+		$("#det_3").slideUp(1000);
+		$(this).hide()
+		$("#show_3").show()
+		
+	});
+	$('#show_4').click(function(){
+		$("#det_4").slideDown(1000);
+		$(this).hide()
+		$("#hide_4").show()
+	});
+	$('#hide_4').click(function(){
+		$("#det_4").slideUp(1000);
+		$(this).hide()
+		$("#show_4").show()
+		
+	});
+	$('#show_5').click(function(){
+		$("#det_5").slideDown(1000);
+		$(this).hide()
+		$("#hide_4").show()
+		
+	});
+	$('#hide_5').click(function(){
+		$("#det_5").slideUp(1000);
+		$(this).hide()
+		$("#show_5").show()
+		
+	});
+});
