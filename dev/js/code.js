0c1a4a81ec127ddf7e8b2c26fcfff73e51e8be02
unknown <mr.1bilous@gmail.com>
2017-11-19 22:17:29 +0200

    add show more/less text

--- dev/js/code.js
+++ dev/js/code.js
@@ -12,88 +12,85 @@ function initMap() {
 	});	
 }
 $(document).ready(function(){
-	
-	$('#show_0').click(function(){
-		$(".short_0").hide()
-		$("#det_0").show()
-		$(this).hide()
-		$("#hide_0").show()	
-	});
-	$('#hide_0').click(function(){
-		$("#det_0").hide()
-		$(this).hide()
-		$("#show_0").show()
-		$(".short_0").show()
-	});
-	
-	$('#show_1').click(function(){
-		$(".short_1").hide()
-		$("#det_1").show()
-		$(this).hide()
-		$("#hide_1").show()	
-	});
-	$('#hide_1').click(function(){
-		$("#det_1").hide()
-		$(this).hide()
-		$("#show_1").show()
-		$(".short_1").show()
-	});
-	
-	$('#show_2').click(function(){
-		$(".short_2").hide()
-		$("#det_2").show()
-		$(this).hide()
-		$("#hide_2").show()	
-	});
-	$('#hide_2').click(function(){
-		$("#det_2").hide()
-		$(this).hide()
-		$("#show_2").show()
-		$(".short_2").show()
-	});
-	
-	$('#show_3').click(function(){
-		$(".short_3").hide()
-		$("#det_3").show()
-		$(this).hide()
-		$("#hide_3").show()	
-	});
-	$('#hide_3').click(function(){
-		$("#det_3").hide()
-		$(this).hide()
-		$("#show_3").show()
-		$(".short_3").show()
-	});
 
-	$('#show_4').click(function(){
-		$(".short_4").hide()
-		$("#det_4").show()
-		$(this).hide()
-		$("#hide_4").show()	
+	$("#show_0").click(function(){
+		$(this).hide();
+		$("#dot_0").css({'max-height': '1000px',
+						});
+		$("#hide_0").show();
 	});
-	$('#hide_4').click(function(){
-		$("#det_4").hide()
-		$(this).hide()
-		$("#show_4").show()
-		$(".short_4").show()
-	});
-
-	$('#show_5').click(function(){
-		$(".short_5").hide()
-		$("#det_5").show()
-		$(this).hide()
-		$("#hide_5").show()	
-	});
-	$('#hide_5').click(function(){
-		$("#det_5").hide()
-		$(this).hide()
-		$("#show_5").show()
-		$(".short_5").show()
+	$("#hide_0").click(function(){
+		$(this).hide();
+		$("#dot_0").css({'max-height': '100px',
+						});
+		$("#show_0").show();
 	});
 	
+	$("#show_1").click(function(){
+		$(this).hide();
+		$("#dot_1").css({'max-height': '1000px',
+						});
+		$("#hide_1").show();
+	});
+	$("#hide_1").click(function(){
+		$(this).hide();
+		$("#dot_1").css({'max-height': '100px',
+						});
+		$("#show_1").show();
+	});
 	
+	$("#show_2").click(function(){
+		$(this).hide();
+		$("#dot_2").css({'max-height': '1000px',
+						});
+		$("#hide_2").show();
+	});
+	$("#hide_2").click(function(){
+		$(this).hide();
+		$("#dot_2").css({'max-height': '100px',
+						});
+		$("#show_2").show();
+	});
 	
+	$("#show_3").click(function(){
+		$(this).hide();
+		$("#dot_3").css({'max-height': '1000px',
+						});
+		$("#hide_3").show();
+	});
+	$("#hide_3").click(function(){
+		$(this).hide();
+		$("#dot_3").css({'max-height': '100px',
+						});
+		$("#show_3").show();
+	});
 	
+	$("#show_4").click(function(){
+		$(this).hide();
+		$("#dot_4").css({'max-height': '1000px',
+						});
+		$("#hide_4").show();
+	});
+	$("#hide_4").click(function(){
+		$(this).hide();
+		$("#dot_4").css({'max-height': '100px',
+						});
+		$("#show_4").show();
+	});
 	
+	$("#show_5").click(function(){
+		$(this).hide();
+		$("#dot_5").css({'max-height': '1000px',
+						});
+		$("#hide_5").show();
+	});
+	$("#hide_5").click(function(){
+		$(this).hide();
+		$("#dot_5").css({'max-height': '100px',
+						});
+		$("#show_5").show();
+	});
 	
 });
+
+    
