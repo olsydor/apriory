b4ae368d5aa1bc79944e5c2ad87c7ccfc033b9dd
unknown <mr.1bilous@gmail.com>
2018-01-25 20:50:54 +0200

    finish apriory

--- dev/js/code.js
+++ dev/js/code.js
@@ -9,87 +9,191 @@ function initMap() {
 	var marker = new google.maps.Marker({
 	  position: mark_in_map,
 	  map: map
-	});	
+	});
 }
 $(document).ready(function(){
-	$("#show_0").click(function(){
-		$(this).hide();
-		$("#dot_0").css({'max-height': '1000px',
-						});
-		$("#hide_0").show();
+	var $dot_0 = $('#dot_0').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	$("#hide_0").click(function(){
-		$(this).hide();
-		$("#dot_0").css({'max-height': '100px',
-						});
-		$("#show_0").show();
+	var $dot_1 = $('#dot_1').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	
-	$("#show_1").click(function(){
-		$(this).hide();
-		$("#dot_1").css({'max-height': '1000px',
-						});
-		$("#hide_1").show();
+	var $dot_2 = $('#dot_2').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	$("#hide_1").click(function(){
-		$(this).hide();
-		$("#dot_1").css({'max-height': '100px',
-						});
-		$("#show_1").show();
+	var $dot_3 = $('#dot_3').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	
-	$("#show_2").click(function(){
-		$(this).hide();
-		$("#dot_2").css({'max-height': '1000px',
-						});
-		$("#hide_2").show();
+	var $dot_4 = $('#dot_4').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	$("#hide_2").click(function(){
-		$(this).hide();
-		$("#dot_2").css({'max-height': '100px',
-						});
-		$("#show_2").show();
+	var $dot_5 = $('#dot_5').dotdotdot({
+		ellipsis: "\u2026 ",
+		keep: '.readmore',
+		callback: function( isTruncated ) {
+			var self =$(this)
+			if (isTruncated) {
+			}else {
+				self.find(".readmore").css("display","none");
+			}
+
+		}
 	});
-	
-	$("#show_3").click(function(){
-		$(this).hide();
-		$("#dot_3").css({'max-height': '1000px',
-						});
-		$("#hide_3").show();
-	});
-	$("#hide_3").click(function(){
-		$(this).hide();
-		$("#dot_3").css({'max-height': '100px',
-						});
-		$("#show_3").show();
-	});
-	
-	$("#show_4").click(function(){
-		$(this).hide();
-		$("#dot_4").css({'max-height': '1000px',
-						});
-		$("#hide_4").show();
-	});
-	$("#hide_4").click(function(){
-		$(this).hide();
-		$("#dot_4").css({'max-height': '100px',
-						});
-		$("#show_4").show();
-	});
-	
-	$("#show_5").click(function(){
-		$(this).hide();
-		$("#dot_5").css({'max-height': '1000px',
-						});
-		$("#hide_5").show();
-	});
-	$("#hide_5").click(function(){
-		$(this).hide();
-		$("#dot_5").css({'max-height': '100px',
-						});
-		$("#show_5").show();
-	});
-	
+	var api_0 = $dot_0.data("dotdotdot");
+	var api_1 = $dot_1.data("dotdotdot");
+	var api_2 = $dot_2.data("dotdotdot");
+	var api_3 = $dot_3.data("dotdotdot");
+	var api_4 = $dot_4.data("dotdotdot");
+	var api_5 = $dot_5.data("dotdotdot");
+	$dot_0.on('click', '.readmore', function( e ){
+			e.preventDefault();
+		
+			//	When truncated, restore
+			if ( $dot_0.hasClass( 'ddd-truncated' ) )
+			{
+				api_0.restore();
+				$dot_0.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_0.removeClass( 'full-story' );
+				api_0.truncate();
+				api_0.watch();
+			}
+		});
+	$dot_1.on('click', '.readmore', function( e ){
+			e.preventDefault();
+		
+			//	When truncated, restore
+			if ( $dot_1.hasClass( 'ddd-truncated' ) )
+			{
+				api_1.restore();
+				$dot_1.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_1.removeClass( 'full-story' );
+				api_1.truncate();
+				api_1.watch();
+			}
+		});
+	$dot_2.on('click', '.readmore', function( e ){
+			e.preventDefault();
+			console.dir(this);
+			//	When truncated, restore
+			if ( $dot_2.hasClass( 'ddd-truncated' ) )
+			{
+				api_2.restore();
+				$dot_2.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_2.removeClass( 'full-story' );
+				api_2.truncate();
+				api_2.watch();
+			}
+		});
+	$dot_3.on('click', '.readmore', function( e ){
+			e.preventDefault();
+		
+			//	When truncated, restore
+			if ( $dot_3.hasClass( 'ddd-truncated' ) )
+			{
+				api_3.restore();
+				$dot_3.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_3.removeClass( 'full-story' );
+				api_3.truncate();
+				api_3.watch();
+			}
+		});
+	$dot_4.on('click', '.readmore', function( e ){
+			e.preventDefault();
+		
+			//	When truncated, restore
+			if ( $dot_4.hasClass( 'ddd-truncated' ) )
+			{
+				api_4.restore();
+				$dot_4.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_4.removeClass( 'full-story' );
+				api_4.truncate();
+				api_4.watch();
+			}
+		});
+	$dot_5.on('click', '.readmore', function( e ){
+			e.preventDefault();
+		
+			//	When truncated, restore
+			if ( $dot_5.hasClass( 'ddd-truncated' ) )
+			{
+				api_5.restore();
+				$dot_5.addClass( 'full-story' );
+			}
+			//	Not truncated, truncate
+			else
+			{
+				$dot_5.removeClass( 'full-story' );
+				api_5.truncate();
+				api_5.watch();
+			}
+		});
+  
+
 });
 
     
